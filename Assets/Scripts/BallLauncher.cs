29d01c7261ac3e6412ebe0af0b91e5f0c43abfee
baourra <baourra99@gmail.com>
2026-05-14 07:03:26 -0700

    Agregar generador de escena base para Physics Test Lab

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -1,33 +1,5 @@
 using UnityEngine;
 
-public class BallLauncher : MonoBehaviour
+public class BallLauncher : LanzadorBola
 {
-    public GameObject bolaPrefab;
-    public Transform puntoSalida;
-    public float fuerzaLanzamiento = 12f;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            LanzarBola();
-        }
-    }
-
-    void LanzarBola()
-    {
-        if (bolaPrefab == null || puntoSalida == null)
-        {
-            return;
-        }
-
-        GameObject nuevaBola = Instantiate(bolaPrefab, puntoSalida.position, Quaternion.identity);
-        Rigidbody rb = nuevaBola.GetComponent<Rigidbody>();
-
-        if (rb != null)
-        {
-            // Aqui usamos impulso para que salga disparada
-            rb.AddForce(transform.forward * fuerzaLanzamiento, ForceMode.Impulse);
-        }
-    }
 }
--- Assets/Scripts/DoorOpener.cs
+++ Assets/Scripts/DoorOpener.cs
@@ -1,30 +1,5 @@
 using UnityEngine;
 
-public class DoorOpener : MonoBehaviour
+public class DoorOpener : AbridorPuerta
 {
-    public bool puertaAbierta = false;
-    public float alturaAbierta = 4f;
-    public float velocidadApertura = 2f;
-
-    private Vector3 posicionCerrada;
-    private Vector3 posicionAbierta;
-
-    void Start()
-    {
-        posicionCerrada = transform.position;
-        posicionAbierta = posicionCerrada + Vector3.up * alturaAbierta;
-    }
-
-    void Update()
-    {
-        if (puertaAbierta)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, posicionAbierta, velocidadApertura * Time.deltaTime);
-        }
-    }
-
-    public void AbrirPuerta()
-    {
-        puertaAbierta = true;
-    }
 }
